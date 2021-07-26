588c7550000dd109eaaded1f6c2b911b7c3a1afa
worldnas <ar.jafari83@gmail.com>
2021-07-26 07:37:43 -0800

    easy

---
+++ Untitled-2.js
@@ -0,0 +1,13 @@
+var name="alireza jafari";
+var email="ar.jafari83@gmail.com";
+var pattren1=/@gmail.com$/g
+var pattren=/^alireza/g
+console.log(pattren.test(name))
+console.log(pattren1.test(email))
+
+
+
+
+
+
+
